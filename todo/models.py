866214a13345b6e1cff6a2eddbefeb4e3920c940
TomOverment Overment <tomozart22@outlook.com>
2024-06-28 11:05:30 +0000

    enable serving of image files

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
